4a62a73f099e5fe4a65a17cf19666b46542028e3
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-04-08 12:29:16 +0000

    Initial commit

---
+++ src/main/java/be/vdab/nieuwefietsen/NieuweFietsenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.nieuwefietsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NieuweFietsenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NieuweFietsenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/nieuwefietsen/NieuweFietsenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.nieuwefietsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NieuweFietsenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
